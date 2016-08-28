61c1802225e5ff61acd4d0ccc690bbaceacb8aeb
Ramiro Trejo <trejoramiro1@gmail.com>
2016-08-28 10:15:47 -0500

    Show route.

--- app/controllers/api/v1/educations_controller.rb
+++ app/controllers/api/v1/educations_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::EducationsController < ApplicationController
     @all_education = Education.find_by(:user_id)
     render 'index.json.jbuilder'
   end
+
+  def show
+    @education = Education.find_by(:id)
+    render 'show.json.jbuilder'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
   namespace :v1 do
     get '/education' => 'educations#index'
+    get '/education/:id' => 'educations#show'
   end
   end
 end

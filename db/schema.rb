552a8f8f9be00578d945205d5b5b7a9db145cffa
Jack Shales <jackshalesmusic@gmail.com>
2016-08-28 10:11:40 -0500

    add route, action, and smaple view

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160828145636) do
+ActiveRecord::Schema.define(version: 20160828145955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_v1_students", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password"
+    t.string   "phone_number"
+    t.text     "bio"
+    t.string   "linked_in_url"
+    t.string   "twitter_handle"
+    t.string   "personal_url"
+    t.string   "resume_url"
+    t.string   "github_url"
+    t.string   "photo"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
--- test/models/api/v1/student_test.rb
+++ test/models/api/v1/student_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class StudentTest < ActiveSupport::TestCase
+class Api::V1::StudentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
